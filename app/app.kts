fd2608ee6e0656585e5b8a379f70e41a6999888d
Maxeem <jmax.posh@gmail.com>
2019-06-22 21:34:55 +0300

    Improvements in wget as text and binary using Kotlin usefull features

--- app/app.kts
+++ app/app.kts
@@ -11,22 +11,24 @@ import java.io.File
 import java.net.URL
 import java.nio.charset.Charset
 
-object Experiences {
+private object Experiences {
     val url = URL("https://poshmark.com/api/meta/experiences")
     val file = File("app.kts.data/experiences.json")
 }
-object Markets {
+private object Markets {
     const val jsonFile = "src/main/res/raw/markets.json"
     const val iconsPath = "src/main/res/mipmap-xxxhdpi"
 }
+private val UTF_8 = Charset.forName("UTF-8")
 
 println("- start")
 //
 kotlin.run {
     println("${Experiences.file} -> isFile: ${Experiences.file.isFile} - length: ${Experiences.file.length()}")
-    (Experiences.file.takeIf { it.length() > 1 } ?: wget(Experiences.url, Experiences.file)).inputStream()
-        .use { input -> JSONParser(JSONParser.MODE_PERMISSIVE).parse(input) }
-    .also { j ->
+    Experiences.file.run {
+        takeUnless {length() > 1}?.apply { wgetAsText(Experiences.url, this) }
+        JSONParser(JSONParser.MODE_PERMISSIVE).parse(readText(UTF_8))
+    }.also { j ->
         val jdmap = mutableMapOf<String, JSONObject>().apply {
             JsonPath.compile("data.*").read<List<JSONObject>>(j)
                 .forEach { put(it.getAsString("short_name"), it) }
@@ -43,7 +45,7 @@ kotlin.run {
             jdmap.get(id)!!.also { jdm-> JSONObject().also { jm ->
                 jout.get("markets").let{ it as JSONArray }.appendElement(jm.appendField("id", id).appendField("label", jdm.getAsString("short_display_name")))
                 Markets.iconsPath.let{File(it, "market_$id.png")}.takeUnless { it.length() > 1}?.run {
-                    wget(URL(jdm.getAsString("img_url_large")), this) }
+                    wgetAsBinary(URL(jdm.getAsString("img_url_large")), this) }
                 JsonPath.compile("content.data.*.id").read<Collection<String>>(jp).forEach { dId: String ->
                     if (id == dId) return@forEach //skip the same market and department like "women" contains "women"
                     if (dId == "wholesale") return@forEach//Wholesale dept. isn't accessible by default for all users and I can't even try/open it
@@ -52,28 +54,27 @@ kotlin.run {
                         jm.run { get("departments") as JSONArray? ?: JSONArray().also { put("departments", it)} }
                             .appendElement(JSONObject().appendField(dId, jdd.getAsString("short_display_name")))
                         Markets.iconsPath.let{File(it, "department_$dId.png")}.takeUnless { it.length() > 1 }?.run {
-                            wget(URL(jdd.getAsString("img_url_large")), this) }
+                            wgetAsBinary(URL(jdd.getAsString("img_url_large")), this) }
                     }
                 }
             }}
         }
         println("jout: " + jout)
-        Markets.jsonFile.let{File(it)}.writer(Charset.forName("UTF-8")).use { jout.writeJSONString(it) }
+        Markets.jsonFile.let { File(it)}.writer(UTF_8).use { jout.writeJSONString(it) }
 //        println(JsonPath.compile("markets[0].departments").read<JSONArray>(jout).get(0).let {it as JSONObject}.entries.first())
     }
-
 }
-fun wget(url: URL, file: File) = url.openStream().buffered().use { bin ->
-        file.outputStream().use { fout ->
-    println(" wget - $url -> $file")
-    ByteArray(8096).apply {
-        var r: Int; do {
-            r = bin.read(this, 0, this.size)
-            if (r > 0) fout.write(this, 0, r)
-        } while (r > 0)
-    }
-    file
-}}
+fun wgetAsText(url: URL, file: File) = file.apply {
+    print("-- wgetAsText - $url -> $file ...")
+    writeText(url.readText(UTF_8), UTF_8)
+    println(" completed -> file size - ${file.length()}")
+}
+fun wgetAsBinary(url: URL, file: File) = file.apply {
+    print("-- wgetAsBinary - $url -> $file ...")
+    url.openStream().copyTo(outputStream())
+    println(" completed -> file size - ${file.length()}")
+}
+//
 //
 println("- end")
 //

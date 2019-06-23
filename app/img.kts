88e47e3f44a78b774d2d10866228f194942c762e
Maxeem <jmax.posh@gmail.com>
2019-06-23 05:05:13 +0300

    Optimized Image Filter object without library dependencies

--- app/img.kts
+++ app/img.kts
@@ -1,52 +1,69 @@
 //
 //
-import java.awt.image.*
+import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
-import kotlin.math.*
 
 kotlin.run {
-    Img.process(File("/home/max/Downloads/market_men.png"), File("/home/max/Downloads/market_men-new.png"))
-//    Img.process(File("/home/max/Downloads/cat1.png"), File("/home/max/Downloads/cat1-new.png"))
-//    Img.process(File("/home/max/Downloads/market_home_a.png"), File("/home/max/Downloads/market_home_a-new.png"))
+    ImgFilter.process(File("/home/max/Downloads/market_men.png"), File("/home/max/Downloads/market_men-new.png"))
+//    ImgFilter.process(java.net.URL("https://d2zlsagv0ouax1.cloudfront.net/assets/poshmarkets/api/market-women@3x-6d69fbd835686076d9fb1169c22ff137.png"), File("/home/max/Downloads/market-women-new.png"))
 }
-private object Img : com.jhlabs.image.PointFilter() {
+object ImgFilter {
     //
     private final val ADJUST_PARAMS = Triple(1.0f, 1.0f, 1.2f)
     private final val BALANCE_PARAMS = Triple(1.0f, 1.0f, 1.4f)
     //
-    fun process(file: File, dest: File) = ImageIO.write(this.filter(ImageIO.read(file), null), "png", dest)
+    fun process(src: File, dest: File) = ImageIO.write(filter(ImageIO.read(src), null), "png", dest)
+    fun process(url: java.net.URL, dest: File) = ImageIO.write(filter(ImageIO.read(url), null), "png", dest)
+    fun process(input: java.io.InputStream, dest: File) = ImageIO.write(filter(ImageIO.read(input), null), "png", dest)
     //
-    override fun filterRGB(x:Int, y:Int, pixel: Int) = Img.balanceColor(pixel, BALANCE_PARAMS).let { it -> Img.adjustImage(it, ADJUST_PARAMS) }
+    private fun filterRGB(x:Int, y:Int, pxl: Int) =
+            balanceColor(pxl, BALANCE_PARAMS).let { it -> adjustImage(it, ADJUST_PARAMS) }
     //
-    private fun adjustImage(pixel: Int, args: Triple<Float, Float, Float>): Int {
+    private fun adjustImage(pxl: Int, args: Triple<Float, Float, Float>): Int {
         val (f1, f2, f3) = args
-        val alpha = pixel.ushr(24)
-        val red = 0x00ff0000.and(pixel).shr(16).times(f3)
-        val green = 0x0000ff00.and(pixel).ushr(8).times(f3)
-        val blue = 0x000000ff.and(pixel).times(f3)
-        val f4 = run { 0.2125.times(red).plus(0.7154.times(green)).plus(0.0721.times(blue)) }.toFloat()
-        val d = f1.toDouble()
-        var r = validate(red.minus(f4).times(f2).plus(f4).minus(0.5).times(d).plus(0.5).toInt())
-        var g = validate(green.minus(f4).times(f2).plus(f4).minus(0.5).times(d).plus(0.5).toInt())
-        var b = value(blue.minus(f4).times(f2).plus(f4).minus(0.5).times(d).plus(0.5).toInt())
-        if (r < 0.0f) {
-            r = 0.0f
-        } else if (r > 255.0f) {
-            r = 255.0f
-        }
-        val f8 = max(0.0f, min(b, 255f))
-        return alpha.shl(24).or(r.toInt().shl(16)).or(g.toInt().shl(8)).or(f8.toInt())
+        val r = r(pxl).times(f3); val g = g(pxl).times(f3); val b = b(pxl).times(f3)
+        val f4 = 0.2125.times(r).plus(0.7154.times(g)).plus(0.0721.times(b))
+        return argb(pxl.ushr(24), r.minus(f4).times(f2).plus(f4).minus(0.5).times(f1).plus(0.5).toInt(),
+                g.minus(f4).times(f2).plus(f4).minus(0.5).times(f1).plus(0.5).toInt(),
+                    b.minus(f4).times(f2).plus(f4).minus(0.5).times(f1).plus(0.5).toInt())
     }
-    private fun balanceColor(pixel: Int, args: Triple<Float, Float, Float>): Int {
-        val (f1, f2, f3) = args
-        val a = pixel.ushr(24)
-        var r = validate(0x00ff0000.and(pixel).shr(16).plus(1).times(f1).toInt())
-        var g = validate(0x0000ff00.and(pixel).shr(8).plus(1).times(f2).toInt())
-        var b = validate(0x000000ff.and(pixel).plus(1).times(f3).toInt().toInt())
-        return a.shl(24).or(r.shl(16)).or(g.shl(8)).or(b)
-    }
+    private fun balanceColor(pxl: Int, args: Triple<Float, Float, Float>) =
+            argb(pxl.ushr(24), r(pxl).plus(1).times(args.first).toInt(),
+                g(pxl).plus(1).times(args.second).toInt(),
+                    b(pxl).plus(1).times(args.third).toInt())
+    private fun r(n: Int) = 0x00ff0000.and(n).shr(16)
+    private fun g(n: Int) = 0x0000ff00.and(n).shr(8)
+    private fun b(n: Int) = 0x000000ff.and(n)
+    private fun argb(a:Int, r:Int, g:Int, b:Int) =
+            a.shl(24).or(validate(r).shl(16)).or(validate(g).shl(8)).or(validate(b))
     private val validate = { i:Int -> when {i in 0..255 -> i; i<0 -> 0; else -> 255 } }
+    //
+    private fun filter(src: BufferedImage, dest: BufferedImage?) : BufferedImage {
+        val (width,height) = src.width to src.height
+        val (type, srcRaster) = src.type to src.raster
+        val dst = dest ?: run {
+            val dstCM = src.colorModel
+            BufferedImage(dstCM, dstCM.createCompatibleWritableRaster(width, height),
+                    dstCM.isAlphaPremultiplied(), null as java.util.Hashtable<*,*>?);
+        }
+        val dstRaster = dst.raster
+        val inPixels = IntArray(width)
+        for (y in 0 until height) {
+            if (type == 2) {
+                srcRaster.getDataElements(0, y, width, 1, inPixels)
+                for (x in 0 until width)
+                    inPixels[x] = filterRGB(x, y, inPixels[x])
+                dstRaster.setDataElements(0, y, width, 1, inPixels)
+            } else {
+                src.getRGB(0, y, width, 1, inPixels, 0, width)
+                for (x in 0 until width)
+                    inPixels[x] = filterRGB(x, y, inPixels[x])
+                dst.setRGB(0, y, width, 1, inPixels, 0, width)
+            }
+        }
+        return dst;
+    }
 }
 //
 //

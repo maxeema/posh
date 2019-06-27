8a55f108ddb998e7f8d2dd593b83b3929a653de5
Maxeem <jmax.posh@gmail.com>
2019-06-27 06:16:46 +0300

    Working with Closet. Handling errors.

--- app/src/main/java/maxeem/america/Closet.kt
+++ app/src/main/java/maxeem/america/Closet.kt
@@ -1,69 +1,67 @@
 package maxeem.america
 
 import android.app.Activity
-import android.content.DialogInterface
 import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Icon
 import android.os.AsyncTask
-import android.text.Editable
-import android.text.InputFilter
-import android.text.InputType
-import android.text.TextWatcher
+import android.text.*
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.ProgressBar
 import androidx.appcompat.app.AlertDialog
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import java.io.FileNotFoundException
 import java.net.URL
 
 object Closet {
-
-    val REGEX = Regex("@?[A-Za-z\\d-_]+")
-
+    //
+    private val REGEX = Regex("@?[A-Za-z\\d-_]+")
+    //
     fun wanna(a: Activity) {
-        lateinit var d : AlertDialog
-        lateinit var t : EditText
+        lateinit var nd : AlertDialog
+        lateinit var ed : EditText
         MaterialAlertDialogBuilder(a)
-        .setTitle(R.string.add_closet)
+        .setTitle(R.string.closet)
         .setView(LinearLayout(a).also { ll ->
             ll.setPadding(U.dpToPxi(30), U.dpToPxi(10), U.dpToPxi(15), U.dpToPxi(5))
             ll.addView(EditText(a).apply {
-                t = this
-                setText("jaw_breaker")
+                ed = this
+                setText("@jaw_breaker")
                 maxLines = 2
                 inputType = InputType.TYPE_CLASS_TEXT.or(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS)
-                filters = filters.plusElement(InputFilter.LengthFilter(15))
-U.log("filter filters")
+                filters = filters.plusElement(InputFilter.LengthFilter(55))
                 addTextChangedListener(object: TextWatcher {
                     override fun afterTextChanged(p0: Editable?) { }
                     override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
                     override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                        d.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = validate(t.text.toString().trim())
+                        nd.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = validate(ed.text.toString().trim())
                     }
                 })
             }, LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT))
-        }).setPositiveButton("Find & Add") { d, v->
-            findAndAdd(a, correct(t.text.toString().trim()))
+        }).setPositiveButton(R.string.add) { d, v->
+            add(a, nd, correct(ed.text.toString().trim()))
         }.create().apply {
-            d = this
-//            setOnShowListener { d->  }
+            nd = this
+            setOnShowListener { d->
+                ed.requestFocus()
+                Selection.setSelection(ed.text, ed.length())
+            }
         }.show()
     }
     //
     private fun validate(name:String) = name.isNotBlank() && name.matches(REGEX)
     private fun correct(name:String) = if (name.startsWith("@")) name.substring(1) else name
     //
-    private fun findAndAdd(a: Activity, name: String) {
-        lateinit var d : AlertDialog
+    private fun add(a: Activity, nd: AlertDialog, name: String) {
         MaterialAlertDialogBuilder(a).setView(ProgressBar(a).apply {
             alpha = .6f
         })
         .setCancelable(false)
-        .create().also {
-            d = it
-            d.setOnShowListener { d ->
-                PinTask(d,"closet_$name", name, "https://poshmark.com/closet/$name") { html->
+        .setBackground(ColorDrawable(Color.parseColor("#99222222")))
+        .create().also { d ->
+            d.setOnShowListener { _ ->
+                PinTask(a, nd, d,"closet_$name", "@$name", "https://poshmark.com/closet/$name") { html->
                     var url = html.substring(html.indexOf("http", html.indexOf("user-image-con", ignoreCase = true), true)).let {
                         it.substringBefore(">").substringBefore("\"").substringBefore("'").substringBefore(" ")//extract img.src attribute value
                     }
@@ -75,30 +73,47 @@ U.log("filter filters")
         }.show()
     }
 
-    private class PinTask(val d: DialogInterface, val id:String, val action:String, val pageUrl:String, val iconExtract: (html:String)->String)
+    private class PinTask(val a: Activity, val nd: AlertDialog, val pd: AlertDialog,
+                  val id:String, val label:String, val pageUrl:String, val iconExtract: (html:String)->String)
             : AsyncTask<Unit, Unit, Any>() {
         override fun doInBackground(vararg p0: Unit?): Any {
             return runCatching {
+                Thread.sleep(500)
                 val iconUrl = iconExtract(URL(pageUrl).readText(U.UTF_8))
                 if (BuildConfig.DEBUG)
                     U.log(" icon url -> $iconUrl")
                 URL(iconUrl).openStream().use { `in`->
-                    return@use S.requestPinned(id, action, Icon.createWithAdaptiveBitmap(BitmapFactory.decodeStream(`in`)))
+                    S.requestPinned(id, label, Icon.createWithAdaptiveBitmap(BitmapFactory.decodeStream(`in`)))
                 }
             }
         }
-        override fun onPostExecute(result: Result<Boolean>) {
-            d.dismiss()
+        override fun onCancelled(result: Any?) {
             if (BuildConfig.DEBUG)
-                U.log(" onPostExecute $result")
-            result.onSuccess { pinned ->
+                U.log(" onCancelled $result")
+            pd.dismiss()
+        }
+        override fun onPostExecute(result: Any) {
+            if (BuildConfig.DEBUG)
+                U.log(" onPostExecute: isCancelled $isCancelled; status $status; pd.isShowing ${pd.isShowing}" +
+                    "\n  result -> $result")
+            if (isCancelled || !pd.isShowing) return
+            pd.dismiss()
+            let { result as Result<Boolean> }.onSuccess { pinned ->
                 if (!pinned)
                     U.toast(R.string.cant_pin)
-            }.onFailure { e -> when(e) {
-                is FileNotFoundException -> TODO()
-                is java.net.UnknownHostException -> TODO()
-                else -> TODO()
-            }}
+            }.onFailure { e ->
+                when (e) {
+                    is java.io.FileNotFoundException -> R.string.not_found
+                    is java.net.UnknownHostException -> R.string.cant_connect
+                    else -> R.string.unlucky
+                }.also { msg ->
+                    MaterialAlertDialogBuilder(a)
+                    .setMessage(Html.fromHtml(U.ctx.getString(msg).plus("<br/>").plus("<small>${e.javaClass.name}</small>"), Html.FROM_HTML_MODE_COMPACT))
+                    .setPositiveButton(R.string.got_it) {d, btn -> d.dismiss()}.create().apply {
+                        setOnDismissListener { nd.show() }
+                    }.show()
+                }
+            }
         }
     }
 
